07aa5b5d3daec6d2e2e733c5d50c96b9d4d0a321
Robin-Law <Robin-Law@users.noreply.github.com>
2017-07-25 00:11:02 -0700

    add suit enum

--- SuitsMe.Core/Models/Card.cs
+++ SuitsMe.Core/Models/Card.cs
@@ -1,3 +1,12 @@
 ﻿namespace SuitsMe.Core.Models
 {
+    //Per bridge rules (Ascending Alphabetical Order)
+    //https://en.wikipedia.org/wiki/High_card_by_suit
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade
+    }
 }
